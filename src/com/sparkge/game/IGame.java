3f6bc24ab7639b9f51e719e1a8b66bc1609d9966
czint <25188880+czint@users.noreply.github.com>
2018-06-02 12:35:46 -0700

    Create IGame and StarFoxLite classes solves #13 and #14 (#23)

---
+++ src/com/sparkge/game/IGame.java
@@ -0,0 +1,10 @@
+package com.sparkge.game;
+
+public interface IGame {
+
+    public void init();
+    public void input();
+    public void update();
+    public void render();
+
+}
---
+++ src/com/sparkge/game/StarFoxLite.java
@@ -0,0 +1,33 @@
+package com.sparkge.game;
+
+public class StarFoxLite implements IGame {
+
+    /**
+     * @description
+     */
+    public void init() {
+
+    };
+
+    /**
+     * @description
+     */
+    public void input() {
+
+    };
+
+    /**
+     * @description
+     */
+    public void update() {
+
+    };
+
+    /**
+     * @description
+     */
+    public void render() {
+
+    };
+
+}
